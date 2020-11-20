d06bfc2c38dbcef7ba2fa84561e2ba6c113b7507
dongzhb <dongzhibo2@rainbowcn.com>
2020-11-20 16:19:25 +0800

    微服务rabbion

---
+++ wywhdgg-ribbon/src/main/java/com/lingzhi/ribbon/WywhdggRibbonApplication.java
@@ -0,0 +1,13 @@
+package com.lingzhi.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WywhdggRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WywhdggRibbonApplication.class, args);
+    }
+
+}
