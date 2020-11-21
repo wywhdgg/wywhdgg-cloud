48aad4b6711decca49be5b069431d773c973c620
dongzhib <dongzhibo@fxiaoke.com>
2020-11-21 17:21:56 +0800

    ribbon

--- wywhdgg-ribbon/src/main/java/com/lingzhi/ribbon/WywhdggRibbonApplication.java
+++ wywhdgg-ribbon/src/main/java/com/lingzhi/ribbon/WywhdggRibbonApplication.java
@@ -2,12 +2,22 @@ package com.lingzhi.ribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class WywhdggRibbonApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WywhdggRibbonApplication.class, args);
+    }
+
+
+    @Bean
+    @LoadBalanced//java.lang.IllegalStateException: No instances available for 127.0.0.1
+    public RestTemplate template(){
+        return new RestTemplate();
     }
 
 }
